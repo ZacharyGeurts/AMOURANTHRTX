d1e1fcc6d0dcff32fbedec0bcee66159a96a0ba0
Zachary Robert Geurts <gzac5314@gmail.com>
2025-10-30 09:33:25 -0400

    Delete include/engine/Vulkan/VulkanRayTracingPipeline.hpp

--- include/engine/Vulkan/VulkanRayTracingPipeline.hpp
+++
@@ -1,9 +0,0 @@
-// include/engine/Vulkan/VulkanRayTracingPipeline.hpp
-#pragma once
-
-#include "engine/Vulkan/VulkanPipelineManager.hpp"
-
-namespace VulkanRTX {
-    // Forward declaration of the method
-    void VulkanPipelineManager::createRayTracingPipeline();
-}
