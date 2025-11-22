1503122d2bb74dfcf70a91c8c42f5e24549ab404
Zachary Robert Geurts <gzac5314@gmail.com>
2025-11-21 21:55:50 -0500

    RKO TOP ROPE

--- include/engine/Vulkan/VkSafeSTypes.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <vulkan/vulkan_core.h>
-
-// STONEKEY v∞ — THE FINAL ETERNAL CONSTANTS — NOVEMBER 17, 2025
-static constinit const VkStructureType kVkWriteDescriptorSetSType = VkStructureType(0x000000013);
-static constinit const VkStructureType kVkWriteDescriptorSetSType_ACCELERATION_STRUCTURE_KHR = VkStructureType(1000268001);
